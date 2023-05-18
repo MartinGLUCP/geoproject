e1e58b166afc5cc15683ee79d1a90112366d8942
MartinGLUCP <mguilbertlejeune@gmail.com>
2023-05-18 14:59:30 +0200

    test of some random function

--- geoproject/__init__.py
+++ geoproject/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Martin Guilbert Lejeune"""
 __email__ = 'mguilbertlejeune@gmail.com'
 __version__ = '0.0.1'
+
+from .geoproject import *
--- geoproject/geoproject.py
+++ geoproject/geoproject.py
@@ -1,1 +1,14 @@
 """Main module."""
+
+import string
+import random
+
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits  # Includes both uppercase and lowercase letters, and digits
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
+
+def generate_lucky_number(length=1):
+    
+    random_string = ''.join(random.choice(string.digits) for _ in range(length))
+    return int(random_string)
