f8b37a8e3532684870137935162b5a2d83df1368
MartinGLUCP <mguilbertlejeune@gmail.com>
2023-05-23 18:20:00 +0200

    setup

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MartinGLUCP/geoproject',
-    version='version='0.0.4',
+    version='0.0.4',
     zip_safe=False,
 )
