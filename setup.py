faad9eaa798eb8f29c7917ab12965d41d2ee29e2
MartinGLUCP <mguilbertlejeune@gmail.com>
2023-05-23 18:12:02 +0200

    Bump version: 0.0.4 → 0.0.4

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MartinGLUCP/geoproject',
-    version='0.0.4',
+    version='version='0.0.4'',
     zip_safe=False,
 )
